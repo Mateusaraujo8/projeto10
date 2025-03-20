a38390da3740d5966204f24910c584e8a01d1935
Mateus Araújo <mathteusferreira.dearaujo@gmail.com>
2025-03-20 14:21:04 -0300

    Nome diferente para evitar conflito

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 
 class User(db.Model):
+    __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), unique=True, nullable=False)
     password = db.Column(db.String(150), nullable=False)
