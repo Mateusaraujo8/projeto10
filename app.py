fa1d9eaa06ca39dd4469e69170142d0aa5bfa27e
Mateus Araújo <mathteusferreira.dearaujo@gmail.com>
2025-03-20 16:45:32 -0300

    teste

--- app.py
+++ app.py
@@ -6,17 +6,10 @@ import os
 from flask_migrate import Migrate
 
 
-
-# No terminal, execute as migrações:
-# flask db init
-# flask db migrate
-# flask db upgrade
-
-
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'supersecretkey'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://advinha_user:IKGEmMcoUGEwjmQmcV1gnDunU7uJfj6e@dpg-cvdvj5hc1ekc73ean28g-a.oregon-postgres.render.com/advinha'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Essa configuração é opcional, mas ajuda a evitar warnings
-
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 migrate = Migrate(app, db)
