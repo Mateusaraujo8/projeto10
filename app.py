7877b0b0514df45bbbe794eacd5e65fc572aa378
Mateus Araújo <mathteusferreira.dearaujo@gmail.com>
2025-03-20 14:14:35 -0300

    update

--- app.py
+++ app.py
@@ -96,5 +96,5 @@ def ranking():
 
 if __name__ == '__main__':
     with app.app_context():
-        db.create_all()
+        db.create_all()  # Cria as tabelas no banco de dados
     app.run(debug=True)
