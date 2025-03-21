a10e7a613219ed7e5797198b18c1146152641aa3
mateusaraujo8 <mathteusferreira.dearaujo@gmail.com>
2025-03-21 16:04:42 -0300

    teste

--- app/__init__.py
+++ app/__init__.py
@@ -1,20 +1,28 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from config import Config
+import os
 
+# Criação do banco de dados
 db = SQLAlchemy()
+
+# Inicializando o Login Manager
 login_manager = LoginManager()
-login_manager.login_view = "main.login"
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object(Config)
-    
+
+    # Configuração do aplicativo
+    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://advinha_db_8h7l_user:bmjqFV8cfmqdEePRM0jojm28M4hJH8qH@dpg-cvep0drtq21c73eghdcg-a/advinha_db_8h7l'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    # Inicializando o banco de dados
     db.init_app(app)
+
+    # Inicializando o Login Manager
     login_manager.init_app(app)
-    
-    from .routes import bp
-    app.register_blueprint(bp)
+
+    # Mais configurações...
 
     return app
--- run.py
+++ run.py
@@ -1,6 +1,8 @@
 from app import create_app
 
+# Criação da aplicação Flask
 app = create_app()
 
+# Inicia o servidor de desenvolvimento
 if __name__ == "__main__":
     app.run(debug=True)
